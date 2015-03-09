1bc519aa765eee1b5c6df4338d55d54d4b3fe2f9
Colm Britton <colmjude@gmail.com>
2015-03-09 09:58:26 +0000

    adding markdown converter

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,7 @@
+var markdown = require( "markdown" ).markdown;
+var fs = require("fs");
+var path = require('path');
+
 module.exports = {
   bind : function (app) {
 
@@ -6,6 +10,18 @@ module.exports = {
     });
 
     // add your routes here
+    app.get('/background-reading', function (req, res) {
+      console.log(path.join(__dirname, 'data/resources.md'));
+      fs.readFile(path.join(__dirname, 'data/resources.md'), {encoding: 'utf-8'}, function (err, data) {
+          if (err) {
+              throw err;
+          }
+
+          //console.log(md(data.toString()));
+          res.send( markdown.toHTML( data ) );
+      });
+      //res.send( markdown.toHTML( "test" ) );
+    });
 
   }
 };
