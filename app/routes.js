7ef49a8886ad211cc75b71dfb52fd3e051ba6ec4
Juan Matthys Uys <opyate@gmail.com>
2015-06-26 11:40:58 +0100

    dynamic prototypes URL

--- app/routes.js
+++ app/routes.js
@@ -6,7 +6,15 @@ module.exports = {
   bind : function (app) {
 
     app.get('/', function (req, res) {
-      res.render('index');
+      var prototypes_url = req.headers.host;
+      if (req.headers.host.indexOf('www.') !== -1) {
+        // we're on www.SUB.DOMAIN.TLD
+        prototypes_url = '//prototypes.' + req.headers.host.substring(4);
+      } else {
+        // we're on the root domain
+        prototypes_url = '//prototypes.' + req.headers.host;
+      }
+      res.render('index', {prototypes_url: prototypes_url});
     });
 
     // add your routes here
