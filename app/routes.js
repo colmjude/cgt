17c98fa81bf610f56ab820be5a0008c4541b42ca
Juan Matthys Uys <opyate@gmail.com>
2015-06-26 15:24:02 +0100

    links

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,11 @@
-var markdown = require( "markdown" ).markdown;
+var markdown = require("markdown").markdown;
 var fs = require("fs");
 var path = require('path');
 
 module.exports = {
-  bind : function (app) {
+  bind: function(app) {
 
-    app.get('/', function (req, res) {
+    app.get('/', function(req, res) {
       var prototypes_url = req.headers.host;
       if (req.headers.host.indexOf('www.') !== -1) {
         // we're on www.SUB.DOMAIN.TLD
@@ -14,27 +14,48 @@ module.exports = {
         // we're on the root domain
         prototypes_url = '//prototypes.' + req.headers.host;
       }
-      res.render('index', {prototypes_url: prototypes_url});
-    });
-
-    // add your routes here
-    app.get('/background-reading', function (req, res) {
-      fs.readFile(path.join(__dirname, 'data/resources.md'), {encoding: 'utf-8'}, function (err, data) {
-          if (err) {
-              throw err;
-          }
-
-          res.render( 'useful_links', {markdown: markdown.toHTML( data )} );
+      res.render('index', {
+        prototypes_url: prototypes_url
       });
     });
 
-    app.get('/user-stories', function (req, res) {
+    // add your routes here
+    app.get('/background-reading', function(req, res) {
+      fs.readFile(path.join(__dirname, 'data/resources.md'), {
+        encoding: 'utf-8'
+      }, function(err, data) {
+        if (err) {
+          throw err;
+        }
+
+        res.render('useful_links', {
+          markdown: markdown.toHTML(data)
+        });
+      });
+    });
+
+    app.get('/user-stories', function(req, res) {
       res.render('user_stories_viewer');
     });
 
-    app.get('/screen', function (req, res) {
+    app.get('/screen', function(req, res) {
       res.render('screen');
     });
 
+    app.get('/apps', function(req, res) {
+      var data;
+      fs.readFile(path.join(__dirname, 'data/apps.json'), {
+        encoding: 'utf-8'
+      }, function(err, data) {
+        if (err) {
+          throw err;
+        } else {
+          data = JSON.parse(data);
+        }
+        res.render('apps', {
+          apps: data
+        });
+      });
+    });
   }
 };
